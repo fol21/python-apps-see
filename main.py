dc325177ccc50ba89700ec811efbd71a021334bb
Fernando <folivelima@gmail.com>
2018-10-25 02:48:45 -0300

    added flask app

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-import src.database.base.baseCRUD as crud
 
-import pymongo
+###### WEB APP ########
+from flask import Flask
+from src.webApp.baseREST import restConfigure
+import jinja2
 
-client = pymongo.MongoClient("mongodb://administrador:see2018@ds027748.mlab.com:27748/see-2018-db")
-mydb = client["see-2018-db"]
+app = Flask(__name__)
 
-db = crud.createDocument(mydb, "customers",{"data":10})
-print(db)
 
-crudObject = crud.BaseCRUD("mongodb://administrador:see2018@ds027748.mlab.com:27748/see-2018-db","see-2018-db")
+my_loader = jinja2.ChoiceLoader([
+            app.jinja_loader,
+            jinja2.FileSystemLoader(['src/webApp/templates']),
+        ])
+app.jinja_loader = my_loader
 
-crudObject.updateDocument("customers",{"data":10}, {"$set":{"data":20}}).createDocument("customers",{"data":30})
+
+restConfigure(app)
+
+if __name__ == '__main__':
+   app.run(port=8085,debug=True)
+
+
+
+
+####### DATABASE #######
+# import src.database.base.baseCRUD as crud
+
+# import pymongo
+
+# client = pymongo.MongoClient("mongodb://administrador:see2018@ds027748.mlab.com:27748/see-2018-db")
+# mydb = client["see-2018-db"]
+
+# db = crud.createDocument(mydb, "customers",{"data":10})
+# print(db)
+
+# crudObject = crud.BaseCRUD("mongodb://administrador:see2018@ds027748.mlab.com:27748/see-2018-db","see-2018-db")
+
+# crudObject.updateDocument("customers",{"data":10}, {"$set":{"data":20}}).createDocument("customers",{"data":30})
 
 
