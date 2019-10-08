1f12d3658c38b69d1f0815e08ca4aef71575f6e6
Fernando Oliveira <folivelima@gmail.com>
2019-10-08 19:13:57 -0300

    +featureREST

--- src/webApp/baseREST.py
+++ src/webApp/baseREST.py
@@ -2,24 +2,25 @@ from flask import render_template, request, jsonify
 
 
 
-def restConfigure(app):
+class BaseREST:
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+    def __init__(self, app):
+        @app.route('/')
+        def index():
+            return render_template('index.html')
 
-    @app.route('/hello/<name>')
-    def hello_name(name):
-        return 'Hello %s!' % name
+        @app.route('/hello/<name>')
+        def hello_name(name):
+            return 'Hello %s!' % name
 
-    @app.route('/api',methods = ['POST', 'GET'])
-    def rest():
-            if request.method == 'POST':
-                obj = {
-                    'name': request.form['name'],
-                    'contact': request.form['contact'],
-                    'email': request.form['email']
-                }
-                return jsonify(obj)
-            else:
-                return  jsonify({"name":request.args.get('name')})
+        @app.route('/api',methods = ['POST', 'GET'])
+        def rest():
+                if request.method == 'POST':
+                    obj = {
+                        'name': request.form['name'],
+                        'contact': request.form['contact'],
+                        'email': request.form['email']
+                    }
+                    return jsonify(obj)
+                else:
+                    return  jsonify({"name":request.args.get('name')})
---
+++ src/webApp/featureREST.py
@@ -0,0 +1,6 @@
+from baseREST import BaseREST
+
+class FeatureREST(BaseREST):
+    
+    def __init__(self, app):
+        super().__init__(app)
